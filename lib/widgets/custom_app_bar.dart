7a32d423c8c602fa72920f510386ae08e98032e3
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2022-03-26 11:48:15 -0300

    feat: add custom_app_bar widget

---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: SvgPicture.asset('assets/icons/menu.svg'),
+            ),
+            const CircleAvatar(
+              backgroundImage: AssetImage('assets/images/avatar.jpeg'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(50);
+}
