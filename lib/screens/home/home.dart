9a91fb3b5bb3aefa48b3bed1c7c57e524ec693ce
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2022-03-26 11:48:37 -0300

    feat: add home screen

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'package:house_rent/widgets/custom_app_bar.dart';
+import 'package:house_rent/widgets/search_input.dart';
+import 'package:house_rent/widgets/welcome_text.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      appBar: const CustomAppBar(),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            WelcomeText(),
+            SearchInput(),
+          ],
+        ),
+      ),
+    );
+  }
+}
